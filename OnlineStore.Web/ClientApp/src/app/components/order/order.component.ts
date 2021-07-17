bfce568280c27a064d3b7f4a6617197b96b3bd5e
XeverDead <xever.dead.115@gmail.com>
2021-07-17 17:15:14 +0300

    Basic validation

--- OnlineStore.Web/ClientApp/src/app/components/order/order.component.ts
+++ OnlineStore.Web/ClientApp/src/app/components/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OrderState } from '../../models/enums/orderState';
 import { Order } from '../../models/order';
+import { Product } from '../../models/product';
 import { OrderService } from '../../services/order.service';
 
 @Component({
@@ -43,5 +44,13 @@ export class OrderComponent implements OnInit {
 
   public delete(): void {
     this.orderService.delete(this.order.id).subscribe();
+  }
+
+  public removeProduct(product: Product): void {
+    let index: number = this.order.products.indexOf(product);
+
+    if (index != -1) {
+      this.order.products.splice(index, 1);
+    }
   }
 }
